1b98ddff886c0579939d4529e7506bf84c3dc519
sourav303 <souravdikshit12@gmail.com>
2026-07-23 00:33:34 +0530

    Update payment-config.js

--- payment-config.js
+++ payment-config.js
@@ -1,6 +1,6 @@
 window.CAPPTONIUS_PAYPAL_CONFIG = {
   // Replace this with your PayPal Business LIVE Client ID before going public.
-  clientId: "REPLACE_WITH_PAYPAL_LIVE_CLIENT_ID",
+  clientId: "AZhGR2_LOZYZEbuLr4NeOQjuw5tgM_zyh6zEflh_VVh9CT3QtvnM3PJpgwmIBWqLzqVAo7ES1aii_L0B",
   currency: "USD",
   businessName: "Capptonius Technology INC",
   paymentDescription: "Capptonius enrollment payment"
